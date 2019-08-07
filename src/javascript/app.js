8da035150ff5595e4ff8cb3c489f384e4bd4d294
Andrew McGrath <27497493+am283721@users.noreply.github.com>
2019-08-06 20:42:50 -0400

    Add multi-level filter support

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -4,6 +4,19 @@ Ext.define("PiAncestorFilterBroadcaster", {
     items: [{
         id: Utils.AncestorPiAppFilter.RENDER_AREA_ID,
         xtype: 'container',
+        layout: {
+            type: 'hbox',
+            align: 'middle',
+            defaultMargins: '0 10 10 0',
+        }
+    }, {
+        id: Utils.AncestorPiAppFilter.PANEL_RENDER_AREA_ID,
+        xtype: 'container',
+        layout: {
+            type: 'hbox',
+            align: 'middle',
+            defaultMargins: '0 10 10 0',
+        }
     }],
 
     config: {
@@ -12,33 +25,33 @@ Ext.define("PiAncestorFilterBroadcaster", {
         }
     },
 
-    launch: function() {
+    launch: function () {
         this.ancestorFilterPlugin = Ext.create('Utils.AncestorPiAppFilter', {
             ptype: 'UtilsAncestorPiAppFilter',
             pluginId: 'ancestorFilterPlugin',
             publisher: true,
+            filtersHidden: false,
             settingsConfig: {
                 labelWidth: 200,
                 margin: 10
             },
             listeners: {
                 scope: this,
-                ready: function(plugin) {
+                ready: function (plugin) {
                     plugin.addListener({
                         scope: this,
-                        select: this._notifySubscribers
+                        select: function () { this._notifySubscribers('ancestor'); },
+                        change: function () { this._notifySubscribers('filters'); }
                     });
-                    this._notifySubscribers()
+                    this._notifySubscribers('ancestor');
                 },
             }
         });
         this.addPlugin(this.ancestorFilterPlugin);
-
-
     },
 
-    _notifySubscribers: function() {
-        this.ancestorFilterPlugin.notifySubscribers();
+    _notifySubscribers: function (changeType) {
+        this.ancestorFilterPlugin.notifySubscribers(changeType);
     },
 
     /**
@@ -46,9 +59,9 @@ Ext.define("PiAncestorFilterBroadcaster", {
      * insert its settings. The SDK decides if an app should have a settings menu option
      * *before* initializing app plugins created in app.launch()
      */
-    getSettingsFields: function() {
+    getSettingsFields: function () {
         return [{
             xtype: 'container'
-        }]
+        }];
     }
 });
